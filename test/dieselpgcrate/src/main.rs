2a8742e2f31ece401c61e50fdd7c2927b9e4fd27
BlackDex <black.dex@gmail.com>
2021-11-21 20:49:00 +0100

    Workflow changes, and rebuild

--- test/dieselpgcrate/src/main.rs
+++ test/dieselpgcrate/src/main.rs
@@ -20,7 +20,7 @@ mod schema {
 }
 
 mod models {
-  use schema::posts;
+  use crate::schema::posts;
   #[derive(Queryable)]
   pub struct Post {
       pub id: i32,
--- test/dieselsqlitecrate/src/main.rs
+++ test/dieselsqlitecrate/src/main.rs
@@ -18,7 +18,7 @@ mod schema {
 }
 
 mod models {
-  use schema::posts;
+  use crate::schema::posts;
   #[derive(Queryable)]
   pub struct Post {
       pub id: i32,
