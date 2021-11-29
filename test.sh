e3c5b738bac4a85f355dc7565387d194b818753a
BlackDex <black.dex@gmail.com>
2021-11-29 16:37:31 +0100

    Fix armv5te build and some cleanup
    It looks like the armv5te build has some issues with OpenSSL.
    Also tested it with the `messense` image, same results.
    Therefore i'm now using a different crate for testing that image.
    
    Also some cleanup and small other items adjusted.

--- test.sh
+++ test.sh
@@ -8,9 +8,9 @@ docker_build() {
     -v "$PWD/test/${crate}:/home/rust/src" \
     -v cargo-cache:/root/.cargo/registry \
     -e RUST_BACKTRACE=1 \
-    -e RUSTFLAGS='-C link-arg=-s' \
+    -e RUSTFLAGS='-Clink-arg=-s' \
     -it "blackdex/rust-musl:x86_64-musl-${RUST_CHANNEL}" \
-    cargo build ${cargo_arg}
+    cargo build --target=x86_64-unknown-linux-musl ${cargo_arg}
 
   cd "test/${crate}" || return
   echo -ne "\n\nTESTING: /target/x86_64-unknown-linux-musl/debug/${crate}\n"
@@ -32,7 +32,7 @@ docker_build_armv7() {
     -v "$PWD/test/${crate}:/home/rust/src" \
     -v cargo-cache:/root/.cargo/registry \
     -e RUST_BACKTRACE=1 \
-    -e RUSTFLAGS='-C link-arg=-s' \
+    -e RUSTFLAGS='-Clink-arg=-s' \
     -it "blackdex/rust-musl:armv7-musleabihf-${RUST_CHANNEL}" \
     cargo build --target=armv7-unknown-linux-musleabihf ${cargo_arg}
 
@@ -79,7 +79,7 @@ docker_build_armhf() {
     -v "$PWD/test/${crate}:/home/rust/src" \
     -v cargo-cache:/root/.cargo/registry \
     -e RUST_BACKTRACE=1 \
-    -e RUSTFLAGS='-C link-arg=-s' \
+    -e RUSTFLAGS='-Clink-arg=-s' \
     -it "blackdex/rust-musl:arm-musleabihf-${RUST_CHANNEL}" \
     cargo build --target=arm-unknown-linux-musleabihf ${cargo_arg}
 
@@ -95,9 +95,32 @@ docker_build_armhf() {
 }
 
 
-MUSL_ARG=
+docker_build_armv5te() {
+  local -r crate="$1"crate
+  local -r cargo_arg="${2}"
+
+  docker run --rm \
+    -v "$PWD/test/${crate}:/home/rust/src" \
+    -v cargo-cache:/root/.cargo/registry \
+    -e RUST_BACKTRACE=1 \
+    -e RUSTFLAGS='-Clink-arg=-s' \
+    -it "blackdex/rust-musl:armv5te-musleabi-${RUST_CHANNEL}" \
+    cargo build --target=armv5te-unknown-linux-musleabi ${cargo_arg}
+
+  cd "test/${crate}" || return
+  echo -ne "\n\nTESTING: /target/armv5te-unknown-linux-musleabi/debug/${crate}\n"
+  qemu-arm -cpu arm1136 ./target/armv5te-unknown-linux-musleabi/debug/"${crate}" ; echo -ne "\nExited with: $?\n"
+  set -x
+  file "target/armv5te-unknown-linux-musleabi/debug/${crate}"
+  ldd "target/armv5te-unknown-linux-musleabi/debug/${crate}"
+  checksec --file="target/armv5te-unknown-linux-musleabi/debug/${crate}"
+  set +x
+  exit 0
+}
+
+
 if [[ -n "${VERBOSE}" ]]; then
-  MUSL_ARG="-vv"
+  MUSL_ARG+=" -vv"
 fi
 
 if [[ -z "${RUST_CHANNEL}" ]]; then
@@ -110,6 +133,8 @@ elif [[ "${ARCH}" == "aarch64" ]]; then
   docker_build_aarch64 "${1}" "${MUSL_ARG}"
 elif [[ "${ARCH}" == "armhf" ]]; then
   docker_build_armhf "${1}" "${MUSL_ARG}"
+elif [[ "${ARCH}" == "armv5te" ]]; then
+  docker_build_armv5te "${1}" "${MUSL_ARG}"
 else
   docker_build "${1}" "${MUSL_ARG}"
 fi
--- test/sslcrate/src/main.rs
+++ test/sslcrate/src/main.rs
@@ -1,12 +1,15 @@
 extern crate openssl;
 use std::str;
-use openssl::hash::{hash, MessageDigest};
+use openssl::{hash::{hash, MessageDigest}, version::{version, platform}};
 
 
 fn main() {
-    let data: &[u8] = b"Hello, world";
+    let data: &[u8] = b"Hello, OpenSSL world";
     let digest = hash(MessageDigest::sha256(), &data);
 
+    println!("version: {}", version());
+    println!("{}", platform());
     println!("{}", str::from_utf8(data).ok().unwrap());
-    println!("hash: {:?}", digest);
+    println!("hash:  {:x?}", digest);
+    println!("sha256sum: d7, 4d, a9, c1, a1, 35, 6a, 18, fd, d1, d7, 48, e8, d8, 8c, 4d, 3d, e2, b6, 3b, 20, 34, 82, ee, 3, 29, d7, 1, 4b, fc, 51, 77");
 }
